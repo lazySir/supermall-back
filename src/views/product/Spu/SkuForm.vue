f4a97f21bed15c8f788c34bd6ec6da3655705634
lazySir <969060742@qq.com>
2022-08-25 10:30:05 +0800

    spu数据
    1.完成skuForm图片数据的收集与展示

--- src/views/product/Spu/SkuForm.vue
+++ src/views/product/Spu/SkuForm.vue
@@ -42,15 +42,29 @@
     </el-form-item>
     <el-form-item label="销售属性">
       <el-form ref="form" label-width="80px">
-        <el-form-item v-for='(saleAttr,index) in spuSaleAttrList' :key='saleAttr.id' :label="saleAttr.saleAttrName">
-          <el-select placeholder="请选择" v-model='saleAttr.attrIdAndValueId'>
-            <el-option v-for='(saleAttrValue,index) in saleAttr.spuSaleAttrValueList' :key='saleAttrValue.id' :label="saleAttrValue.saleAttrValueName" :value="`${saleAttr.id}:${saleAttrValue.id}`"></el-option>
+        <el-form-item
+          v-for="(saleAttr, index) in spuSaleAttrList"
+          :key="saleAttr.id"
+          :label="saleAttr.saleAttrName"
+        >
+          <el-select placeholder="请选择" v-model="saleAttr.attrIdAndValueId">
+            <el-option
+              v-for="(saleAttrValue, index) in saleAttr.spuSaleAttrValueList"
+              :key="saleAttrValue.id"
+              :label="saleAttrValue.saleAttrValueName"
+              :value="`${saleAttr.id}:${saleAttrValue.id}`"
+            ></el-option>
           </el-select>
         </el-form-item>
       </el-form>
     </el-form-item>
     <el-form-item label="图片列表">
-      <el-table border style="width: 100%">
+      <el-table
+        @selection-change="handleSelectionChange"
+        :data="spuImageList"
+        border
+        style="width: 100%"
+      >
         <el-table-column
           prop="prop"
           type="selection"
@@ -59,16 +73,28 @@
         >
         </el-table-column>
         <el-table-column prop="prop" label="图片" width="width">
+          <template slot-scope="{ row, $index }">
+            <img :src="row.imgUrl" style="height: 100px; width: 100px" alt="" />
+          </template>
         </el-table-column>
-        <el-table-column prop="prop" label="名称" width="width">
+        <el-table-column prop="imgName" label="名称" width="width">
         </el-table-column>
         <el-table-column prop="prop" label="操作" width="width">
+          <template slot-scope="{ row, $index }">
+            <el-button
+              v-if="row.isDefault == 0"
+              @click="changeDefault(row)"
+              type="primary"
+              >设为默认</el-button
+            >
+            <el-button type="success">默认</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </el-form-item>
     <el-form-item>
       <el-button type="primary">保存</el-button>
-      <el-button >取消</el-button>
+      <el-button>取消</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -130,6 +156,8 @@ export default {
       },
       //存储spu
       spu: {},
+      //临时收集图片table选中的数据  未处理 缺少isDefault
+      imageList: [],
     };
   },
   methods: {
@@ -143,7 +171,11 @@ export default {
       //获取spu图片的数据
       let result = await this.$API.sku.reqSpuImageList(spu.id);
       if (result.code == 200) {
-        this.spuImageList = result.data;
+        let list = result.data;
+        list.array.forEach((item) => {
+          item.isDefault = 0;
+        });
+        this.spuImageList = list;
       }
       //获取销售属性的数据
       let result1 = await this.$API.sku.reqSpuSaleAttrList(spu.id);
@@ -160,6 +192,22 @@ export default {
         this.attrInfoList = result2.data;
       }
     },
+    //图片table表格复选框按钮的事件
+    handleSelectionChange(params) {
+      //获取到用户选中的图片信息，但是需要注意的是 当前收集的数据当中缺少isDefault字段（服务器需要）
+      this.imageList = params;
+    },
+    //图片table设为默认  排他操作
+    changeDefault(row){
+    //图片列表的数据isDefault 变为0
+    this.spuImageList.forEach(item=>{
+      item.isDefault=0;
+    })
+    //点击的那个图片数据变为1
+    row.isDefault=1
+    //收集默认图片的地址
+    this.skuInfo.skuDefaultImg=row.imgUrl
+    }
   },
 };
 </script>
